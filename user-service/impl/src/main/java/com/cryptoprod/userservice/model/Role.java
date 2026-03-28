d9154bbde5fb2ed8ecb59582cc1ca76dba96291d
vanish <ivanmaxi52@gmail.com>
2026-03-28 22:38:37 +0300

    создание юзер сервиса, добавил liquibase и модель юзера

---
+++ user-service/impl/src/main/java/com/cryptoprod/userservice/model/Role.java
@@ -0,0 +1,6 @@
+package com.cryptoprod.userservice.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ user-service/impl/src/main/java/com/cryptoprod/userservice/model/User.java
@@ -0,0 +1,51 @@
+package com.cryptoprod.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "account")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "UUID")
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+
+}
